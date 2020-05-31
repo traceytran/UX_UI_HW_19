dee173312f371b411e3c7e627e04c4832bb16c03
traceytran <58285786+traceytran@users.noreply.github.com>
2020-05-30 23:47:57 -0700

    Commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+console.log("Your index.js file is loaded correctly!");
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("topnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
